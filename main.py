a9d4f9645ad3ca8f91a09abdc72c6dfa4ff98e91
huydang <dangqhuy1999@gmail.com>
2024-11-19 19:48:14 +0700

    venv ne

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ async def load_page(url):
     
     #processing
     print(await page.content())
-    #toi comment ne tui nuafdfdf 
+    #toi comment ne tui 
     await browser.close()
 
 async def read_urls_from_file(filename):
